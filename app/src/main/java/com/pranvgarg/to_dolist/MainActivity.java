17ef957068971971bca32a008dbd69d1fe5df9bb
Pranav Garg <pranvgarg@Pranavs-MacBook-Air.local>
2021-05-09 15:08:35 +0530

    Initial commit

---
+++ app/src/main/java/com/pranvgarg/to_dolist/MainActivity.java
@@ -0,0 +1,202 @@
+package com.pranvgarg.to_dolist;
+
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewClickInterface {
+
+    private static final String TAG = "MainActivity";
+
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<String> moviesList;
+
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        moviesList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(moviesList, this);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerAdapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        moviesList.add("Iron Man");
+        moviesList.add("The Incredible Hulk");
+        moviesList.add("Iron Man 2");
+        moviesList.add("Thor");
+        moviesList.add("Captain America: The First Avenger");
+        moviesList.add("The Avengers");
+        moviesList.add("Iron Man 3");
+        moviesList.add("Thor: The Dark World");
+        moviesList.add("Captain America: The Winter Soldier");
+        moviesList.add("Guardians of the Galaxy");
+        moviesList.add("Avengers: Age of Ultron");
+        moviesList.add("Ant-Man");
+        moviesList.add("Captain America: Civil War");
+        moviesList.add("Doctor Strange");
+        moviesList.add("Guardians of the Galaxy Vol. 2");
+        moviesList.add("Spider-Man: Homecoming");
+        moviesList.add("Thor: Ragnarok");
+        moviesList.add("Black Panther");
+        moviesList.add("Avengers: Infinity War");
+        moviesList.add("Ant-Man and the Wasp");
+        moviesList.add("Captain Marvel");
+        moviesList.add("Avengers: Endgame");
+        moviesList.add("Spider-Man: Far From Home");
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                moviesList.add("Black Widow (2020)");
+                moviesList.add("The Eternals (2020)");
+                moviesList.add("Shang-Chi and the Legend of the Ten Rings (2021)");
+                moviesList.add("Doctor Strange in the Multiverse of Madness (2021)");
+                moviesList.add("Thor: Love and Thunder (2021)");
+
+                recyclerAdapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+    }
+
+    String deletedMovie = null;
+    List<String> archivedMovies = new ArrayList<>();
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+
+            final int position = viewHolder.getAdapterPosition();
+
+            switch (direction) {
+                case ItemTouchHelper.LEFT:
+                    deletedMovie = moviesList.get(position);
+                    moviesList.remove(position);
+                    recyclerAdapter.notifyItemRemoved(position);
+                    Snackbar.make(recyclerView, deletedMovie, Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    moviesList.add(position, deletedMovie);
+                                    recyclerAdapter.notifyItemInserted(position);
+                                }
+                            }).show();
+                    break;
+                case ItemTouchHelper.RIGHT:
+                    final String movieName = moviesList.get(position);
+                    archivedMovies.add(movieName);
+
+                    moviesList.remove(position);
+                    recyclerAdapter.notifyItemRemoved(position);
+
+                    Snackbar.make(recyclerView, movieName + ", Archived.", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    archivedMovies.remove(archivedMovies.lastIndexOf(movieName));
+                                    moviesList.add(position, movieName);
+                                    recyclerAdapter.notifyItemInserted(position);
+                                }
+                            }).show();
+
+                    break;
+            }
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(MainActivity.this, c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorAccent))
+                    .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                    .addSwipeRightBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark))
+                    .addSwipeRightActionIcon(R.drawable.ic_baseline_archive_24)
+                    .setActionIconTint(ContextCompat.getColor(recyclerView.getContext(), android.R.color.white))
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
+
+    @Override
+    public void onItemClick(int position) {
+
+        Toast.makeText(this, moviesList.get(position),Toast.LENGTH_LONG).show();
+//        Intent intent = new Intent(this, NewActivity.class);
+//        intent.putExtra("MOVIE_NAME", moviesList.get(position));
+//        startActivity(intent);
+    }
+
+    @Override
+    public void onLongItemClick(final int position) {
+//        moviesList.remove(position);
+//        recyclerAdapter.notifyItemRemoved(position);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.LogOut:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/pranvgarg/to_dolist/RecyclerViewClickInterface.java
@@ -0,0 +1,6 @@
+package com.pranvgarg.to_dolist;
+
+public interface RecyclerViewClickInterface {
+    void onItemClick(int position);
+    void onLongItemClick(int position);
+}
